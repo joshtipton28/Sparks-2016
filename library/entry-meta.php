06a9d3a8f54e6705184d9de55850ff527678255a
Josh Tipton <josh@blue-bot.com>
2016-03-09 20:34:27 -0500

    Custom Entry Meta
    Customized Post entry meta and included gravatar image

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -9,8 +9,8 @@
 
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
-		echo '<time class="updated" datetime="'. get_the_time( 'c' ) .'">'. sprintf( __( 'Posted on %s at %s.', 'foundationpress' ), get_the_date(), get_the_time() ) .'</time>';
-		echo '<p class="byline author">'. __( 'Written by', 'foundationpress' ) .' <a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+		echo '<p class="byline author">' . '<span class="avatar-img">' . get_avatar( get_the_author_meta( 'ID' ), 54) . '</span>' . __( 'By', 'foundationpress' ) .' <a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'" rel="author" class="fn">'. get_the_author() .'</a> | ';
+		echo '<time class="updated" datetime="'. get_the_time( 'c' ) .'">'. sprintf( __( '%s %s', 'foundationpress' ), get_the_date(), get_the_time() ) .'</time></p>';
 	}
 endif;
 ?>
