2c234043118423bca78f7f4165eb6048b593ba8f
Josh Tipton <josh@blue-bot.com>
2016-04-07 12:37:50 -0400

    Journal Fixes

--- parts/journal-footer.php
+++ parts/journal-footer.php
@@ -12,7 +12,7 @@
 ?>
 
 		<div class="medium reveal fast" id="journal-learn-more" data-reveal data-overlay="false" data-animation-in="slide-in-up" data-animation-out="slide-out-down">
-			<p>Form Goes Here</p>
+			<?php echo do_shortcode('[gravityform id="2" title="false" description="false"]'); ?>
 
 			<button class="close-button" data-close aria-label="Close modal" type="button">
 			  <span aria-hidden="true">&times;</span>
