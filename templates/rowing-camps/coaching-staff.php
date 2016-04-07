d669a26d8b1fc5c486d5c96a7f4939870547ba69
Josh Tipton <josh@blue-bot.com>
2016-04-07 10:17:06 -0400

    Additional fixes to Cowing Camps listings

--- templates/rowing-camps/coaching-staff.php
+++ templates/rowing-camps/coaching-staff.php
@@ -2,54 +2,52 @@
 /**
  * Template part for Rowing Camps Custom Post Type
  **/
+
 ?>
 <div class="tabs-panel" id="camp-staff">
 	<h2><?php echo $campTitle;?>
 		<em>
 			<?php echo $campLocation;?>
 
-			<?php 
+			<?php
 				if ($programType == 'program') echo 'Program Staff';
                 else echo 'Coaching Staff'; ?>
+
 		</em>
 	</h2>
-	
-	<?php if( have_rows('coaching_staff') ): ?>
 
-    <?php while( have_rows('coaching_staff') ): the_row(); ?>
-        
-        <?php 
-        
-	        $coachName = get_sub_field('coach_name'); 
-	        $coachCreds = get_sub_field('coach_credentials');
-	        $coachBio = get_sub_field('coach_bio');
-	        $coachPic = get_sub_field('coach_picture');
-        
-        ?>
+    	<?php $staff = get_field('staff'); ?>
+			<?php if ($staff) : ?>
 
-        <div class="coaching-staff">
-	        <h4><?php echo $coachName;?></h4>
+    		<?php foreach ($staff as $s) : ?>
 
-	        
-	        
-	        <?php if( !empty($coachPic) ): ?>
+    			<?php
+    				$coachName = get_the_title( $s->ID );
+	    			$coachCreds = get_field('staff_credentials', $s->ID);
+			        $coachBio = get_field('staff_bio', $s->ID);
+			        $coachPic = get_field('staff_photo', $s->ID);
+			    ?>
 
-				<img src="<?php echo $coachPic['url']; ?>" alt="<?php echo $coachPic['alt']; ?>" />
+    			<div class="coaching-staff">
 
-			<?php endif; ?>
+			        <h4><?php echo $coachName; ?></h4>
 
-			<h5><?php echo $coachCreds;?></h5>
 
-			<p><?php echo $coachBio;?></p>
+			        <?php if( !empty($coachPic) ): ?>
 
-        </div>
+						<img src="<?php echo $coachPic['url']; ?>" alt="<?php echo $coachPic['alt']; ?>" />
 
-        <hr>
-        
-    <?php endwhile; ?>
+					<?php endif; ?>
 
-  
- 
- 
-	<?php endif; ?>
+					<h5><?php echo $coachCreds;?></h5>
+
+					<p><?php echo $coachBio;?></p>
+
+		        </div>
+
+		        <hr>
+
+    		<?php endforeach; ?>
+    	<?php endif; ?>
+
 </div>
--- templates/rowing-camps/overview.php
+++ templates/rowing-camps/overview.php
@@ -2,28 +2,31 @@
 /**
  * Template part for off canvas menu
  **/
+//Get Media
+$overviewImage = get_field('featured_image');
+$overviewVideo = get_field('featured_video');
+
 ?>
 	<div class="tabs-panel is-active" id="camp-overview">
 		<h1><?php echo $campTitle;?> <em><?php the_field('camp_name');?> Overview</em></h1>
-		
+
 		<div class="featured-media">
 			<?php
 
-				if(get_field('featured_image'))
-				{
-					echo '<p>' . get_field('featured_image') . '</p>';
-				}
+				if(!empty($overviewImage)) : ?>
 
-			?>
+				<img src="<?php echo $overviewImage['url']; ?>" alt="<?php echo $overviewImage['alt']; ?>" >
+
+			<?php endif; ?>
 
 			<?php
 
-				if(get_field('featured_video'))
-				{
-					echo '<p>' . get_field('featured_video') . '</p>';
-				}
-				
-			?>
+				if(!empty($overviewVideo)): ?>
+
+					<p> <?php echo $overviewVideo; ?> </p>
+
+				<?php endif; ?>
+
 		</div>
 
 		<?php the_field('overview_content'); ?>
