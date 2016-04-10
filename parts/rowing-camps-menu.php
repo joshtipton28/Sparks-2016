b9ec2b5a7fa83a173cf6b031040c7120584b8e94
Josh Tipton <josh@blue-bot.com>
2016-04-09 22:20:00 -0400

    Rowing Camps Mobile Menu Styling

--- parts/rowing-camps-menu.php
+++ parts/rowing-camps-menu.php
@@ -1,31 +1,71 @@
 <div class="camp-accordion large-4 columns">
-  <h3><?php the_title();?></h3>
-    <ul class="accordion" data-accordion data-allow-all-closed="true">
-      <li class="accordion-item" data-accordion-item>
-        <a class="accordion-title">Summer Camps<i class="fa fa-chevron-right"></i></a>
 
-            <?php dynamic_sidebar('summer-menu'); ?>
+  <div class="tab-accordion show-for-small-only">
+    <ul class="accordion" data-accordion-menu>
+        <li class="accordion-item" data-accordion-item>
+            <a href="#" class="accordion-title"><h3>Select Camp Type<i class="fa fa-chevron-down"></i></h3></a>
 
-      </li>
-      <li class="accordion-item" data-accordion-item>
-        <a class="accordion-title">Winter Camps<i class="fa fa-chevron-right"></i></a>
+                <ul class="menu vertical nested accordion" data-accordion data-allow-all-closed="true">
+                  <li class="accordion-item" data-accordion-item>
+                    <a class="accordion-title">Summer Camps<i class="fa fa-chevron-right"></i></a>
 
-            <?php dynamic_sidebar('winter-menu'); ?>
+                        <?php dynamic_sidebar('summer-menu'); ?>
 
-      </li>
-      <li class="accordion-item" data-accordion-item>
-        <a class="accordion-title">International Programs<i class="fa fa-chevron-right"></i></a>
+                  </li>
+                  <li class="accordion-item" data-accordion-item>
+                    <a class="accordion-title">Winter Camps<i class="fa fa-chevron-right"></i></a>
 
-            <?php dynamic_sidebar('development-programs-menu'); ?>
-      </li>
+                        <?php dynamic_sidebar('winter-menu'); ?>
 
+                  </li>
+                  <li class="accordion-item" data-accordion-item>
+                    <a class="accordion-title">International Programs<i class="fa fa-chevron-right"></i></a>
+
+                        <?php dynamic_sidebar('development-programs-menu'); ?>
+                  </li>
+
+                </ul>
+                <ul class="menu vertical nested accordion">
+                  <li class="accordion-item">
+                    <a class="accordion-title map-button" href="<?php echo get_home_url(); ?>/map-of-camp-locations">
+                      Camp Dates and Locations<i class="fa fa-chevron-right"></i>
+                    </a>
+                  </li>
+                </ul>
+        </li>
     </ul>
-    <ul class="accordion">
-      <li class="accordion-item">
-        <a class="accordion-title map-button" href="<?php echo get_home_url(); ?>/map-of-camp-locations">
-          Camp Dates and Locations<i class="fa fa-chevron-right"></i>
-        </a>
-      </li>
-    </ul>
-
   </div>
+
+
+
+  <div class="hide-for-small-only">
+    <h3><?php the_title();?></h3>
+      <ul class="accordion" data-accordion data-allow-all-closed="true">
+        <li class="accordion-item" data-accordion-item>
+          <a class="accordion-title">Summer Camps<i class="fa fa-chevron-right"></i></a>
+
+              <?php dynamic_sidebar('summer-menu'); ?>
+
+        </li>
+        <li class="accordion-item" data-accordion-item>
+          <a class="accordion-title">Winter Camps<i class="fa fa-chevron-right"></i></a>
+
+              <?php dynamic_sidebar('winter-menu'); ?>
+
+        </li>
+        <li class="accordion-item" data-accordion-item>
+          <a class="accordion-title">International Programs<i class="fa fa-chevron-right"></i></a>
+
+              <?php dynamic_sidebar('development-programs-menu'); ?>
+        </li>
+
+      </ul>
+      <ul class="accordion">
+        <li class="accordion-item">
+          <a class="accordion-title map-button" href="<?php echo get_home_url(); ?>/map-of-camp-locations">
+            Camp Dates and Locations<i class="fa fa-chevron-right"></i>
+          </a>
+        </li>
+      </ul>
+  </div>
+</div>
