4029b457740d8a5065866d7a834a4501f6fd060b
Josh Tipton <josh@blue-bot.com>
2016-04-09 22:19:50 -0400

    Tab Pages Mobile Menu Styling

--- templates/working-with-us/tab-menu.php
+++ templates/working-with-us/tab-menu.php
@@ -5,18 +5,41 @@
 ?>
 <div class="row">
     <div class="tabs-wrap medium-4 columns">
-      <h3><?php the_title();?></h3>
-      <ul class="tabs vertical" data-tabs id="working-with-us">
-        <li class="tabs-title is-active"><a href="#college-counseling" aria-selected="true">College Counseling<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#coxswain-program">Coxswain Program<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#international-applicants">International Applicants<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#gap-year-advising">Gap Year Advising<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#reports-from-the-front">Reports From The Front<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#our-approach">Our Approach<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#counseling-testimonials">Counseling Testimonials<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#rowing-camp-testimonials">Camp Testimonials<i class="fa fa-chevron-right"></i></a></li>
-        <li class="tabs-title"><a href="#coxswain-testimonials">Coxswain Testimonials<i class="fa fa-chevron-right"></i></a></li>
-      </ul>
+        <div class="tab-accordion show-for-small-only">
+            <ul class="accordion" data-accordion data-allow-all-closed="true">
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title"><h3><?php the_title();?><i class="fa fa-chevron-down"></i></h3></a>
+                        <div class="accordion-content" data-tab-content>
+                          <ul class="tabs vertical" data-tabs id="working-with-us">
+                            <li class="tabs-title is-active"><a href="#college-counseling" aria-selected="true">College Counseling<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#coxswain-program">Coxswain Program<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#international-applicants">International Applicants<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#gap-year-advising">Gap Year Advising<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#reports-from-the-front">Reports From The Front<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#our-approach">Our Approach<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#counseling-testimonials">Counseling Testimonials<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#rowing-camp-testimonials">Camp Testimonials<i class="fa fa-chevron-right"></i></a></li>
+                            <li class="tabs-title"><a href="#coxswain-testimonials">Coxswain Testimonials<i class="fa fa-chevron-right"></i></a></li>
+                          </ul>
+                        </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="hide-for-small-only">
+          <h3><?php the_title();?></h3>
+          <ul class="tabs vertical" data-tabs id="working-with-us">
+            <li class="tabs-title is-active"><a href="#college-counseling" aria-selected="true">College Counseling<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#coxswain-program">Coxswain Program<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#international-applicants">International Applicants<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#gap-year-advising">Gap Year Advising<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#reports-from-the-front">Reports From The Front<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#our-approach">Our Approach<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#counseling-testimonials">Counseling Testimonials<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#rowing-camp-testimonials">Camp Testimonials<i class="fa fa-chevron-right"></i></a></li>
+            <li class="tabs-title"><a href="#coxswain-testimonials">Coxswain Testimonials<i class="fa fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
     </div>
 
     <div class="nano has-scrollbar tabs-content vertical medium-8 columns" data-tabs-content="working-with-us">
