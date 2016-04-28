18b218473bf06f902093d83f1b5f2730a5c96d4f
Josh Tipton <josh@blue-bot.com>
2016-04-27 20:20:46 -0700

    Revert "Moving Event Tracking scripts to Foundation.js"
    This reverts commit ae761c2aff2a02d7bf3f738b2d3f92ffb072f957.

--- header.php
+++ header.php
@@ -22,6 +22,16 @@
 		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Roboto+Slab:400,700|Days+One' rel='stylesheet' type='text/css'>
 
+		<script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-13277703-1', 'auto');
+                ga('send', 'pageview');
+
+                </script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
