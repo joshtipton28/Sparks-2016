6a216d0fb5c10a052e5f42a6b454e129405c44cb
Josh Tipton <josh@blue-bot.com>
2016-04-28 19:29:08 -0700

    Adding  accordion to Rowing Camps Mobile

--- parts/rowing-camps-menu.php
+++ parts/rowing-camps-menu.php
@@ -2,36 +2,40 @@
 
   <?php /*Mobile Accordion Menu */ ?>
   <div class="tab-accordion hide-for-large">
+    <ul class="accordion" data-accordion  data-allow-all-closed="true">
+      <li class="accordion-item is-active" data-accordion-item>
+        <a href="#" class="accordion-title"><h3><?php the_title();?><i class="fa fa-chevron-down"></i></h3></a>
+          <div class="accordion-content" data-tab-content>
+            <ul class="menu vertical nested accordion" data-accordion data-allow-all-closed="true">
+              <li class="accordion-item" data-accordion-item>
+                <a class="accordion-title">Summer Camps<i class="fa fa-chevron-right"></i></a>
 
+                    <?php dynamic_sidebar('summer-menu'); ?>
 
-                <ul class="menu vertical nested accordion" data-accordion data-allow-all-closed="true">
-                  <li class="accordion-item" data-accordion-item>
-                    <a class="accordion-title">Summer Camps<i class="fa fa-chevron-right"></i></a>
+              </li>
+              <li class="accordion-item" data-accordion-item>
+                <a class="accordion-title">Winter Camps<i class="fa fa-chevron-right"></i></a>
 
-                        <?php dynamic_sidebar('summer-menu'); ?>
+                    <?php dynamic_sidebar('winter-menu'); ?>
 
-                  </li>
-                  <li class="accordion-item" data-accordion-item>
-                    <a class="accordion-title">Winter Camps<i class="fa fa-chevron-right"></i></a>
+              </li>
+              <li class="accordion-item" data-accordion-item>
+                <a class="accordion-title">International Programs<i class="fa fa-chevron-right"></i></a>
 
-                        <?php dynamic_sidebar('winter-menu'); ?>
+                    <?php dynamic_sidebar('development-programs-menu'); ?>
+              </li>
 
-                  </li>
-                  <li class="accordion-item" data-accordion-item>
-                    <a class="accordion-title">International Programs<i class="fa fa-chevron-right"></i></a>
-
-                        <?php dynamic_sidebar('development-programs-menu'); ?>
-                  </li>
-
-                </ul>
-                <ul class="menu vertical nested accordion">
-                  <li class="accordion-item">
-                    <a class="accordion-title map-button" href="<?php echo get_home_url(); ?>/map-of-camp-locations">
-                      Camp Dates and Locations<i class="fa fa-chevron-right"></i>
-                    </a>
-                  </li>
-                </ul>
-
+            </ul>
+            <ul class="menu vertical nested accordion">
+              <li class="accordion-item">
+                <a class="accordion-title map-button" href="<?php echo get_home_url(); ?>/map-of-camp-locations">
+                  Camp Dates and Locations<i class="fa fa-chevron-right"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+      </li>
+    </ul>
   </div>
 
 
