c742174bdeda5c32003e384faa4b76ce8c72355e
Josh Tipton <josh@blue-bot.com>
2016-07-13 21:12:40 -0400

    Adding Content Box to Rowing Camps Page

--- templates/rowing-camps.php
+++ templates/rowing-camps.php
@@ -12,7 +12,20 @@ get_header(); ?>
 
 	  <div class="row">
 	    <?php include(locate_template('parts/rowing-camps-menu.php' )); ?>
-	  </div>
+
+	        	<div class="camps-tab tabs-content nano has-scrollbar large-8 columns end" data-tabs-content="rowing-camp-tabs">
+			        <div class="nano-content">
+			        	<h2><?php the_title(); ?></h2>
+			        	<hr>
+			        		<?php if ( have_posts() ) : ?>
+								<?php while ( have_posts() ) : the_post(); ?>
+									<?php the_content(); ?>
+								<?php endwhile; ?>
+							<?php endif; ?>
+			        </div>
+		        </div>
+
+
 	</div>
 
 <?php do_action( 'foundationpress_after_content' ); ?>
